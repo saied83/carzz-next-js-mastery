e8c28af6981247dd5a2671ba98c0bb5a84fe05f3
saied83 <saiedtechit@gmail.com>
2024-09-16 15:41:39 +0600

    finish header footer and hero section

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import CustomButton from "./CustomButton";
+
+export { Hero, CustomButton, Navbar, Footer };
